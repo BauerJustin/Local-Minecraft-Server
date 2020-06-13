5a22afcbfc1a0bfe8468700e7e808c83d6736f42
Justin Bauer <39811522+BauerJustin@users.noreply.github.com>
2020-06-13 17:20:14 -0600

    Update to check if local ip address has changed

--- start-server.py
+++ start-server.py
@@ -3,28 +3,32 @@ import subprocess, os, urllib.request, csv, socket
 # Connection variables
 port = 25565
 path = r'{}\start-server.bat'.format(os.getcwd())
-external_ip_address = urllib.request.urlopen('https://ident.me').read().decode('utf8')
-local_ip_address = socket.gethostbyname(socket.gethostname()) 
+local_ip_address = socket.gethostbyname(socket.gethostname())
+external_ip_address = urllib.request.urlopen('https://ident.me').read().decode('utf8') 
 
 # Launch text for server operator
 def launch_instructions():
-       new_ip = False
+       new_local_ip = False
+       new_external_ip = False
        # Open ip_address text file to see if external ip address has change
        with open("ip_address.txt") as file:
-          ip = next(csv.reader(file))
-          new_ip = ip[0] != external_ip_address
+          local_ip, external_ip = next(csv.reader(file))
+          new_local_ip = local_ip != local_ip_address
+          new_external_ip = external_ip != external_ip_address
        # Text displayed to user
        print("/---------------------------------------/")
        print("Launch Instructions:\n")
        print("To launch from your PC type localhost in \ndirect connect\n")
+       if new_local_ip:
+              print("***WARNING NEW LOCAL IP ADDRESS***\n")
        print("For local players on the same network \nlaunch using {}\n".format(local_ip_address))
-       if new_ip:
+       if new_external_ip:
               print("***WARNING NEW EXTERNAL IP ADDRESS***\n")
-              # Update text file with new ip address
-              with open("ip_address.txt", "w") as file:
-                     file.write(external_ip_address)
        print("For external players launch using \n{}:{}\n".format(external_ip_address,port)) 
        print("/---------------------------------------/")
+       # Update text file with new ip addresses
+       with open("ip_address.txt", "w") as file:
+              file.write("{},{}".format(local_ip_address, external_ip_address))
 
 launch_instructions()
 
