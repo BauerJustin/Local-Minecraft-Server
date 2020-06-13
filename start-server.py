6a69797ae831795ed8e4ce091963c085ede12a4c
Justin Bauer <39811522+BauerJustin@users.noreply.github.com>
2020-06-12 20:40:03 -0600

    added local IP address

--- start-server.py
+++ start-server.py
@@ -1,9 +1,10 @@
-import subprocess, os, urllib.request, csv
+import subprocess, os, urllib.request, csv, socket       
 
 # Connection variables
 port = 25565
 path = r'{}\start-server.bat'.format(os.getcwd())
-ip_address = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+external_ip_address = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+local_ip_address = socket.gethostbyname(socket.gethostname()) 
 
 # Launch text for server operator
 def launch_instructions():
@@ -11,19 +12,20 @@ def launch_instructions():
        # Open ip_address text file to see if external ip address has change
        with open("ip_address.txt") as file:
           ip = next(csv.reader(file))
-          if ip[0] != ip_address:
+          if ip[0] != external_ip_address:
                  new_ip = True
        # Text displayed to user
-       print("/----------------------------------------/")
+       print("/---------------------------------------/")
        print("Launch Instructions:\n")
-       print("To launch from your PC type localhost in \ndirect connect within multiplayer in game\n")
+       print("To launch from your PC type localhost in \ndirect connect\n")
+       print("For local players on the same network \nlaunch using {}\n".format(local_ip_address))
        if new_ip:
-              print("***WARNING NEW IP ADDRESS***\n")
+              print("***WARNING NEW EXTERNAL IP ADDRESS***\n")
               # Update text file with new ip address
               with open("ip_address.txt", "w") as file:
-                     file.write(ip_address)
-       print("For external players launch using \n{}:{}\n".format(ip_address,port)) 
-       print("/----------------------------------------/")
+                     file.write(external_ip_address)
+       print("For external players launch using \n{}:{}\n".format(external_ip_address,port)) 
+       print("/---------------------------------------/")
 
 launch_instructions()
 
